d569c60d202daea92d01e4a2ae3caec7835e709d
Norrismi <Norrismi0@gmail.com>
2021-08-24 13:45:49 -0400

    added about page styles and other styles

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,9 @@ import rastaMenu from '../Assets/rastaMenu.JPG'
 import salmonMenu from '../Assets/salmonMenu.jpg'
 import brownMenu from '../Assets/brownMenu.jpeg'
 
+import { faCarrot } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const About = () => {
     return (
@@ -11,45 +14,81 @@ const About = () => {
             <div className={styles.about_titleContainer}>
 
                 <h1 className={styles.about_title}>{`DT's Kitchen`}</h1>
-                <h3 className={styles.about_title}>How we got strated!</h3>
+                <h3 className={styles.about_title}>How we got started!</h3>
             </div>
 
-            <div className={styles.row}>
-                <div className={styles.row_picture}>
+            <div className={styles.aboutIcon_container}>
+
+                <FontAwesomeIcon icon={faCarrot} className={styles.icon} />
+                <FontAwesomeIcon icon={faCarrot} className={styles.icon} />
+                <FontAwesomeIcon icon={faCarrot} className={styles.icon} />
+            </div>
+
+
+            <div className={styles.info_group}>
+                <div className={styles.row_paragraph}>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae blanditiis odit, itaque, reprehenderit porro error doloremque vero doloribus sunt ipsum, repellat nemo? Deleniti culpa id dolores nam sunt, libero quas?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae blanditiis odit, itaque, reprehenderit porro error doloremque vero doloribus sunt ipsum, repellat nemo? Deleniti culpa id dolores nam sunt, libero quas?
+
+                </div>
+                <div className={styles.imageContainer_rasta}>
                     <Image src={rastaMenu} alt="example dinner menu" />
                 </div>
-                <div className={styles.row_paragraph}>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae blanditiis odit, itaque, reprehenderit porro error doloremque vero doloribus sunt ipsum, repellat nemo? Deleniti culpa id dolores nam sunt, libero quas?
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae blanditiis odit, itaque, reprehenderit porro error doloremque vero doloribus sunt ipsum, repellat nemo? Deleniti culpa id dolores nam sunt, libero quas?
 
+                <div className={styles.imageContainer_rasta_mobile}>
+                    <Image src={rastaMenu} alt="example dinner menu" />
                 </div>
 
             </div>
 
 
-            <div className={styles.row}>
+            <div className={styles.info_group}>
+                <div className={styles.imageContainer_salmon}>
+                    <Image src={salmonMenu} alt="example dinner menu" />
+                </div>
                 <div className={styles.row_paragraph}>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae blanditiis odit, itaque, reprehenderit porro error doloremque vero doloribus sunt ipsum, repellat nemo? Deleniti culpa id dolores nam sunt, libero quas?
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae blanditiis odit, itaque, reprehenderit porro error doloremque vero doloribus sunt ipsum, repellat nemo? Deleniti culpa id dolores nam sunt, libero quas?
 
                 </div>
-                <div className={styles.row_picture}>
+
+                <div className={styles.imageContainer_salmon_mobile}>
                     <Image src={salmonMenu} alt="example dinner menu" />
                 </div>
 
             </div>
 
-            <div className={styles.row}>
-                <div className={styles.row_picture}>
-                    <Image src={brownMenu} alt="example dinner menu" />
-                </div>
+            <div className={styles.info_group}>
                 <div className={styles.row_paragraph}>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae blanditiis odit, itaque, reprehenderit porro error doloremque vero doloribus sunt ipsum, repellat nemo? Deleniti culpa id dolores nam sunt, libero quas?
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae blanditiis odit, itaque, reprehenderit porro error doloremque vero doloribus sunt ipsum, repellat nemo? Deleniti culpa id dolores nam sunt, libero quas?
 
                 </div>
+                <div className={styles.imageContainer_brown}>
+                    <Image src={brownMenu} alt="example dinner menu" />
+                </div>
+
+                <div className={styles.imageContainer_brown_mobile}>
+                    <Image src={brownMenu} alt="example dinner menu" />
+                </div>
 
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         </div>
     );
