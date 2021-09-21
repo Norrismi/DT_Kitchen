b7cf102d576da472f9e5ff4da6dedfa7039a1e2b
Norrismi <Norrismi0@gmail.com>
2021-09-21 12:42:17 -0400

    changed O in order form

--- pages/orderForm.js
+++ pages/orderForm.js
@@ -2,20 +2,20 @@ import Image from 'next/image'
 import MultiStepForm from '../components/Form/MultiStepForm';
 import CarouselPics from '../components/CarouselPics';
 
-const orderForm = () => {
-    // return (
-    //     <div className={styles.home_bannerContainer}>
-    //     <div className={styles.home_bannerContainer}>
+const OrderForm = () => {
+    return (
+        <div className={styles.home_bannerContainer}>
+        <div className={styles.home_bannerContainer}>
   
-    //       <Image src={dt_kitchen_spice} className={`${styles.home_banner} `} alt="Intro Banner" />
-    //     </div>
+          <Image src={dt_kitchen_spice} className={`${styles.home_banner} `} alt="Intro Banner" />
+        </div>
   
-    //     <MultiStepForm />
+        <MultiStepForm />
   
-    //     <CarouselPics />
+        <CarouselPics />
   
-    //   </div>
-    // );
+      </div>
+    );
 }
 
-export default orderForm;
+export default OrderForm;
