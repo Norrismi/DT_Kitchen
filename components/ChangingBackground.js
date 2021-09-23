e378ee5fc69ba6c7abaf2572f2d5a01209ced56b
Norrismi <Norrismi0@gmail.com>
2021-09-23 11:52:25 -0400

    changed order background

---
+++ components/ChangingBackground.js
@@ -0,0 +1,23 @@
+
+
+// import React from 'react';
+
+// const ChangingBackground = () => {
+//     const isBackgroundRed = true;
+
+//     return (
+//       <div
+//         style={{
+//           backgroundColor: isBackgroundRed ? 'red' : 'black',
+//           width: '100px',
+//           height: '100px'
+//         }}
+//       />
+//     );
+//   }
+// export default ChangingBackground;
+
+
+
+
+
--- pages/orderForm.js
+++ pages/orderForm.js
@@ -1,33 +1,35 @@
 import Image from 'next/image'
 import MultiStepForm from '../components/Form/MultiStepForm';
 import CarouselPics from '../components/CarouselPics';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/OrderForm.module.css'
 import dt_kitchen_spice from '../Assets/dt_kitchen_spice.png'
 import SmokeElement from "smoke-effect-react";
+import ChangingBackground from '../components/ChangingBackground';
 
 
 
 const OrderForm = () => {
   return (
-    <div className={styles.home_bannerContainer}>
-      <div className={styles.home_bannerContainer}>
+    <div className={styles.orderForm_Container}>
+      <div className={styles.orderForm_bannerContainer}>
 
         <Image src={dt_kitchen_spice} className={`${styles.home_banner} `} alt="Intro Banner" />
       </div>
 
       <MultiStepForm />
 
-      <SmokeElement
+      {/* <SmokeElement
         className={styles.home_smokeBackground}
         opacity="1"
         smokeSrc="https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png"
         smokeOpacity="0.5"
         width="1000"
        
-      />
+      /> */}
 
       <CarouselPics />
 
+     
     </div>
   );
 }
